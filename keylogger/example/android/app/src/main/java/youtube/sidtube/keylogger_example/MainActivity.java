5dab4b6125620fbcff0ed3d9d8333875aa8e8889
cimplesid <joshisid178@gmail.com>
2020-06-19 01:55:20 +0545

    removed unnecesssary directory

--- keylogger/example/android/app/src/main/java/youtube/sidtube/keylogger_example/MainActivity.java
+++
@@ -1,9 +0,0 @@
-package youtube.sidtube.keylogger_example;
-
-import android.content.Intent;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-
-}
--- keylogger/ios/Classes/KeyloggerPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface KeyloggerPlugin : NSObject<FlutterPlugin>
-@end
