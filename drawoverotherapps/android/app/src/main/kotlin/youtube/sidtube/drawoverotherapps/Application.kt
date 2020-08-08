727b06fa8c4adbf958fa25d9c374fdc650085f96
cimplesid <joshisid178@gmail.com>
2020-08-08 14:53:17 +0545

    plugin setup

---
+++ drawoverotherapps/android/app/src/main/kotlin/youtube/sidtube/drawoverotherapps/Application.kt
@@ -0,0 +1,14 @@
+import `in`.jvapps.system_alert_window.SystemAlertWindowPlugin
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
+class Application : FlutterApplication(), PluginRegistrantCallback {
+    override fun onCreate() {
+        super.onCreate()
+        SystemAlertWindowPlugin.setPluginRegistrant(this)
+    }
+
+   override fun registerWith(registry: PluginRegistry) {
+      SystemAlertWindowPlugin.registerWith(registry.registrarFor("in.jvapps.system_alert_window"));
+   }
+}
